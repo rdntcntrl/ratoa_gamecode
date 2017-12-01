d4cb693ae316696c73f27b56098a9e032fa6d0d9
Rodent Control <rdnt.cntrl@localhost>
2017-12-01 00:00:00 +0000

    reset rampjump after teleport

--- code/game/g_misc.c
+++ code/game/g_misc.c
@@ -111,6 +111,9 @@ void TeleportPlayer( gentity_t *player, vec3_t origin, vec3_t angles ) {
 	// toggle the teleport bit so the client knows to not lerp
 	player->client->ps.eFlags ^= EF_TELEPORT_BIT;
 
+	// reset rampjump
+	player->client->ps.stats[STAT_JUMPTIME] = 0;
+
 //unlagged - backward reconciliation #3
 	// we don't want players being backward-reconciled back through teleporters
 	G_ResetHistory( player );
