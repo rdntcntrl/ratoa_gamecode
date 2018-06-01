6d2665d8a6d4d2eabb5c6c9c5b9cd7b441872b5c
Rodent Control <rdnt.cntrl@localhost>
2018-06-01 00:00:00 +0000

    reduce team switch time

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -1221,7 +1221,7 @@ void Cmd_Team_f( gentity_t *ent ) {
 	
 	if( !force ) {
 	    if ( !level.timeout && ent->client->switchTeamTime > level.time ) {
-		    trap_SendServerCommand( ent-g_entities, "print \"May not switch teams more than once per 5 seconds.\n\"" );
+		    trap_SendServerCommand( ent-g_entities, "print \"May not switch teams more than once per 3 seconds.\n\"" );
 		    return;
 		}
 	}
@@ -1236,7 +1236,7 @@ void Cmd_Team_f( gentity_t *ent ) {
 
 	SetTeam( ent, s );
 
-	ent->client->switchTeamTime = level.time + 5000;
+	ent->client->switchTeamTime = level.time + 3000;
 }
 
 
