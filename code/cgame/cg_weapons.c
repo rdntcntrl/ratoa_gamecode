6711d0c7b5b6d1bd588fe38d94901426c836976c
Rodent Control <rdnt.cntrl@localhost>
2018-06-01 00:00:00 +0000

    remove unneeded FireWeapon block in elimination mode

--- code/cgame/cg_weapons.c
+++ code/cgame/cg_weapons.c
@@ -3227,8 +3227,8 @@ void CG_FireWeapon( centity_t *cent ) {
 	int				c;
 	weaponInfo_t	*weap;
 
-	if((cgs.gametype == GT_ELIMINATION || cgs.gametype == GT_CTF_ELIMINATION) && cgs.roundStartTime>cg.time && cg.warmup == 0)
-		return; //if we havn't started in ELIMINATION then do not fire
+	//if((cgs.gametype == GT_ELIMINATION || cgs.gametype == GT_CTF_ELIMINATION) && cgs.roundStartTime>cg.time && cg.warmup == 0)
+	//	return; //if we havn't started in ELIMINATION then do not fire
 
 	ent = &cent->currentState;
 	if ( ent->weapon == WP_NONE ) {
