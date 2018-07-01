b83829a6164e2d430420e659c3a3dc2b2a758961
Rodent Control <rdnt.cntrl@localhost>
2018-07-01 00:00:00 +0000

    add TREASURE HUNTER gametype

--- code/game/ai_dmq3.c
+++ code/game/ai_dmq3.c
@@ -116,7 +116,7 @@ untrap_BotGetLevelItemGoal
 ==================
  */
 int untrap_BotGetLevelItemGoal(int start, char *classname, void /* struct bot_goal_s */ *goal) {
-    static char *gametypeNames[] = {"ffa", "tournament", "single", "team", "ctf", "oneflag", "obelisk", "harvester", "elimination", "ctf", "lms", "dd", "dom"};
+    static char *gametypeNames[] = {"ffa", "tournament", "single", "team", "ctf", "oneflag", "obelisk", "harvester", "elimination", "ctf", "lms", "dd", "dom", "th"};
     char allowedGametypes[MAX_EPAIRKEY];
     char *gametypeName;
 
