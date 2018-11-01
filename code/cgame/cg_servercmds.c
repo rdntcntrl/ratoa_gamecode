43792b99a55a8443dda4a6482c8114a8fbd74fa4
Rodent Control <rdnt.cntrl@localhost>
2018-11-01 00:00:00 +0000

    reduce centerprint font size

--- code/cgame/cg_servercmds.c
+++ code/cgame/cg_servercmds.c
@@ -1590,7 +1590,8 @@ static void CG_ServerCommand( void ) {
 	}
 
 	if ( !strcmp( cmd, "cp" ) ) {
-		CG_CenterPrint( CG_Argv(1), SCREEN_HEIGHT * 0.30, BIGCHAR_WIDTH );
+		//CG_CenterPrint( CG_Argv(1), SCREEN_HEIGHT * 0.30, BIGCHAR_WIDTH );
+		CG_CenterPrint( CG_Argv(1), SCREEN_HEIGHT * 0.30, CENTERPRINT_WIDTH );
 		return;
 	}
 
