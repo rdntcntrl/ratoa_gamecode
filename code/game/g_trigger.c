8b431ffe729a8a0593e1486c6f0e634c41fd3399
Rodent Control <rdnt.cntrl@localhost>
2019-06-01 00:00:00 +0000

    g_gravityModifier no longer affects the jumppad aiming

--- code/game/g_trigger.c
+++ code/game/g_trigger.c
@@ -173,7 +173,8 @@ void AimAtTarget( gentity_t *self ) {
 	}
 
 	height = ent->s.origin[2] - origin[2];
-	gravity = g_gravity.value*g_gravityModifier.value;
+	//gravity = g_gravity.value*g_gravityModifier.value;
+	gravity = g_gravity.value;
 	time = sqrt( height / ( .5 * gravity ) );
 	if ( !time ) {
 		G_FreeEntity( self );
