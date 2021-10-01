26d8e08f47eea744e1dbf93cc8b2238da88ccfb2
Rodent Control <rdnt.cntrl@localhost>
2021-10-01 00:00:00 +0000

    reset cg.eventSequence properly when it advances too far
    This should fix "double predicted event" bugs (causing sounds to be
    played twice) if cg_checkChangedEvents is on.
    Relates to #20

--- code/cgame/cg_predict.c
+++ code/cgame/cg_predict.c
@@ -1139,11 +1139,12 @@ void CG_PredictPlayerState( void ) {
 	// fire events and other transition triggered things
 	CG_TransitionPlayerState( &cg.predictedPlayerState, &oldPlayerState );
 
-	if ( cg_showmiss.integer ) {
-		if (cg.eventSequence > cg.predictedPlayerState.eventSequence) {
+	if ( (cg_checkChangedEvents.integer || cg_showmiss.integer) &&
+			cg.eventSequence > cg.predictedPlayerState.eventSequence) {
+		if (cg_showmiss.integer) {
 			CG_Printf("WARNING: double event\n");
-			cg.eventSequence = cg.predictedPlayerState.eventSequence;
 		}
+		cg.eventSequence = cg.predictedPlayerState.eventSequence;
 	}
 
 	CG_EncodePlayerBBox(&cg_pmove, &cg.predictedPlayerEntity.currentState);
