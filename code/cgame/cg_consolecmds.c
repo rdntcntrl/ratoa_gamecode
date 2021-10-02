af34e49fa3be0fbd8e6108784815391f903dc0c5
Joey Herguth <oitzujoey@gmail.com>
2021-10-02 15:48:22 -0700

    Remove double smooth stairs entry in rules.

--- code/cgame/cg_consolecmds.c
+++ code/cgame/cg_consolecmds.c
@@ -330,7 +330,6 @@ static void CG_Rules_f( void ) {
 	CG_Printf(" -Smooth stairs:         %s\n", CG_OnOffStr(cgs.ratFlags & RAT_SMOOTHSTAIRS));
 	CG_Printf(" -Crouch slide:          %s\n", CG_OnOffStr(cgs.ratFlags & RAT_CROUCHSLIDE));
 	CG_Printf(" -Crouch slide mode:     %s\n", (cgs.ratFlags & RAT_SLIDEMODE) ? "liberal" : "conservative");
-	CG_Printf(" -Smooth stairs:         %s\n", CG_OnOffStr(cgs.ratFlags & RAT_SMOOTHSTAIRS));
 	CG_Printf(" -Overbounce:            %s\n", CG_OnOffStr(!(cgs.ratFlags & RAT_NOOVERBOUNCE)));
 	CG_Printf(" -Fast weapon switch:    %s\n", CG_OnOffStr(cgs.ratFlags & RAT_FASTSWITCH));
 	CG_Printf(" -Fast weapons:          %s\n", CG_OnOffStr(cgs.ratFlags & RAT_FASTWEAPONS));
