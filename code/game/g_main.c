890a4493873d06a9225dff57f9417b2d733fed75
Rodent Control <rdnt.cntrl@localhost>
2022-10-01 00:00:00 +0000

    unset unused CVAR_SERVERINFO for g_maxGameClients

--- code/game/g_main.c
+++ code/game/g_main.c
@@ -439,7 +439,7 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_gametype, "g_gametype", "0", CVAR_SERVERINFO | CVAR_USERINFO | CVAR_LATCH, 0, qfalse  },
 
 	{ &g_maxclients, "sv_maxclients", "8", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, 0, qfalse  },
-	{ &g_maxGameClients, "g_maxGameClients", "0", CVAR_SERVERINFO | CVAR_LATCH | CVAR_ARCHIVE, 0, qfalse  },
+	{ &g_maxGameClients, "g_maxGameClients", "0", CVAR_LATCH | CVAR_ARCHIVE, 0, qfalse  },
 
 	// change anytime vars
 	{ &g_dmflags, "dmflags", "0", CVAR_SERVERINFO | CVAR_ARCHIVE, 0, qtrue  },
