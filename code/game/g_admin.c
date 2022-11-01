a4108834e7f902b86b9e404c2c7a78bb1d8b8084
Rodent Control <rdnt.cntrl@localhost>
2022-11-01 00:00:00 +0000

    level >= 3 admins should not get floodrestricted

--- code/game/g_admin.c
+++ code/game/g_admin.c
@@ -714,7 +714,7 @@ static void admin_level2_flags(char *flags, int size) {
 static void admin_level3_flags(char *flags, int size) {
 	admin_level2_flags(flags, size);
 	Q_strcat(flags, size,
-			va("%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
+			va("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
 				ADMF_RESTART,
 				ADMF_KICK,
 				ADMF_MUTE,
@@ -728,7 +728,8 @@ static void admin_level3_flags(char *flags, int size) {
 				ADMF_TEAMS,
 				ADMF_ALLREADY,
 				ADMF_TIMEOUT,
-				ADMF_WARN
+				ADMF_WARN,
+				ADMF_NOCENSORFLOOD
 			  )
 		);
 }
